1ef2271251549d17b59f8d6e3f050b76c8cdb6c3
abody114 <beesoole@hotmail.com>
2020-05-20 16:51:47 +0300

    string

--- kiim.py
+++ kiim.py
@@ -5,3 +5,14 @@ print(c)
 
 t = "abody"
 print(t)
+
+fname = "abody"
+lname = 'ali'
+print(fname.upper() +" "+lname.lower()) 
+
+flname = 'abody ali'
+print(flname.replace('ali' , 'mohamed'))
+
+b = 'a' * 500
+print(b)
+
