14b8c9ddab61717d86d0c9153527bd3f7c0909a6
abody114 <beesoole@hotmail.com>
2020-05-21 18:15:34 +0300

    first commit

---
+++ oparetors.py
@@ -0,0 +1,22 @@
+num1 = 10
+num2 = 15
+print(num1 > num2)
+print(num2 > num1)
+print(5 == 6)
+print(5 == 5)
+print(5 != 6)
+print(5 >= 6)
+print(6 >= 6)
+print(5 <= 6)
+
+print(True and False)
+print(True and True)
+print(False and False)
+
+print(True or True)
+print(True or False)
+print(False or False)
+
+print(not True)
+print(not False)
+
