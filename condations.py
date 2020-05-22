d7a756add9e5c91077ff6a65fdb89faa2884feca
abody114 <beesoole@hotmail.com>
2020-05-22 23:56:24 +0300

    conditions

---
+++ condations.py
@@ -0,0 +1,6 @@
+username = input("prlease insert your username:")
+password = input("please insert your password")
+if username == 'abody'and password ==  '123':
+    print('welcome your email')
+else:
+    print('eror')
