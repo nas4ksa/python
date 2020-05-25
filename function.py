f5c6b5282d5cccf3b2170d652cf931e0edcf3b34
= <=>
2020-05-25 15:05:14 +0300

    function

---
+++ function.py
@@ -0,0 +1,14 @@
+def liner():
+    print('...................')
+
+def addtion(n1,n2):
+    print(int(n1) + int(n2))
+
+n1 = input("please insert first number: ")
+n2 = input('please insert second number: ') 
+addtion(n1,n2)
+
+#print('abody ali')
+#liner()
+#print(30)
+#liner()
