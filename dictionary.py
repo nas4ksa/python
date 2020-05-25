d90592660a758542d69ccdb90b0d6a5592748ce4
= <=>
2020-05-25 18:05:35 +0300

    dictionaryo

---
+++ dictionary.py
@@ -0,0 +1,8 @@
+subjects_with_no_students = {"arabic" :3, "enghlish" :3, "math" :2}
+print(subjects_with_no_students)
+students = {"arabic":["abody","mohamad","hasan"],"enghlish":["ali","omar","ibrahim"],"math":["hiba","ahmad"] }
+print(students)
+students['math']=('salah','samir')
+print(students)
+students.update({'history':'fadi'})
+print(students)
